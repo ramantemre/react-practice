b4fb97594bf9a360193e7b88bf5c73537ded650a
Raman Temre <ramantemre@gmail.com>
2023-04-17 16:29:18 +0530

    feat: usage of <StrictMode>

--- src/index.js
+++ src/index.js
@@ -6,9 +6,17 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//   <React.StrictMode>
+        <App /> 
+//   </React.StrictMode>
+
+//  StrictMode cause the double logging, re-rendering of component 
+
+// Strict Mode enables the following development-only behaviors:
+// Your components will re-render an extra time to find bugs caused by impure rendering.
+// Your components will re-run Effects an extra time to find bugs caused by missing Effect cleanup.
+// Your components will be checked for usage of deprecated APIs.
+// Reference: https://react.dev/reference/react/StrictMode 
 );
 
 // If you want to start measuring performance in your app, pass a function
